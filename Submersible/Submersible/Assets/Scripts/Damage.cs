43c226e54ede7cb10e19520b052510db8dcb2646
Mitchell Smith <gtgalaxiyt@gmail.com>
2016-07-31 21:11:16 +1000

    Add existing file

---
+++ Submersible/Submersible/Assets/Scripts/Damage.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+	public float health = 10.0f;
+	int damageAmount;
+	int damageTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health == 0.0f) {
+			Destroy (gameObject);
+			Debug.Log ("Player destroyed");
+		}
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Enemy")
+		{
+			StartCoroutine (DamageOverTimeCoroutine (1f));
+		}
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
+
+		StopAllCoroutines();
+
+	}
+
+	IEnumerator DamageOverTimeCoroutine(float wait)
+	{
+		while (health >= 0)
+		{
+			health -= 1.0f;
+			Debug.Log (health);
+			yield return new WaitForSeconds (1f);
+		}
+
+	}
+
+}
+
---
+++ Submersible/Submersible/Assets/Scripts/Movementplayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movementplayer : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		var move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+
+	}
+}
