a159740dc89a20203493ee1f753aa60103e61706
Jack Floyd <jackadamfloyd@gmail.com>
2016-08-08 20:40:00 +1000

    Fish enemy 1 movement and rotation complete

---
+++ Project/Submersible/Assets/Scripts/Enemymovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemymovement : MonoBehaviour
+{
+
+
+	void Start ()
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+
+	
+		if(transform.position.x > -30.5)
+		{
+			transform.localRotation = Quaternion.Euler (0, 0, 0);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Waypoint1") 
+		{
+			transform.localRotation = Quaternion.Euler (0, 180, 0);
+		}
+		if (col.tag == "Waypoint2") 
+		{
+			transform.localRotation = Quaternion.Euler (0, 0, 0);
+		}
+	}
+
+	}
