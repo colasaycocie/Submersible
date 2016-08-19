8f211a1c56eb10de46784b2829744895bffc13f8
Cola Saycocie <colasaycocie@gmail.com>
2016-08-19 12:57:35 +1000

    Added Playmaker to project, completed main menu's and started on gameplay UI

---
+++ Project/Submersible/Assets/Scenes/Menus/MainTitle/ButtonExit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonExit : MonoBehaviour {
+
+	public void QuitGame()
+	{
+
+
+		Application.Quit ();
+		Debug.Log ("Quit");
+
+
+
+	}
+}
