e990d244e6c64647af911731379ce5f6ea155b69
Cola Saycocie <colasaycocie@gmail.com>
2016-08-19 17:16:54 +1000

    Health bar ui and pickups

---
+++ Project/Submersible/Assets/Scripts/Collector.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour {
+
+
+	private int numberofCoins = 0;
+	public Text numberofCoinsText;
+
+	public int torpedosAmount = 0;
+	public Text numberofTorpedosText;
+
+	public AudioClip coinSound;
+	public AudioClip ammoSound;
+	public AudioClip healthSound;
+	private AudioSource pickupAudio;
+
+	public Damage myHealthScript;
+	public float healthIncrease = 1f;
+
+	public HealthBar myHealthBar;
+
+
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		pickupAudio = GetComponent<AudioSource> ();
+
+		myHealthScript = myHealthScript.GetComponent<Damage> ();
+
+		myHealthBar = myHealthBar.GetComponent<HealthBar> ();
+
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Coins")) 
+		{
+
+			pickupAudio.clip = coinSound;
+			pickupAudio.Play ();
+
+			Destroy (other.gameObject);
+			++numberofCoins;
+
+			numberofCoinsText.text = numberofCoins.ToString ();
+
+			Debug.Log ("Number of Coins: " + numberofCoins);
+
+		}
+
+
+		if (other.gameObject.CompareTag ("Ammo")) 
+		{
+
+			pickupAudio.clip = ammoSound;
+			pickupAudio.Play ();
+
+			Destroy (other.gameObject);
+			++torpedosAmount;
+
+			numberofTorpedosText.text = torpedosAmount.ToString ();
+
+			Debug.Log ("Number of Torpedos: " + torpedosAmount);
+
+		}
+
+		if (other.gameObject.CompareTag ("Health") && myHealthBar.Health.fillAmount < 1f) 
+		{
+			pickupAudio.clip = healthSound;
+			pickupAudio.Play ();
+
+			myHealthScript.health += healthIncrease;
+
+			myHealthBar.GainHealth ();
+
+			Debug.Log ("Health : " + healthIncrease);
+
+			Destroy (other.gameObject);
+
+		}
+
+	}
+}
---
+++ Project/Submersible/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour {
+
+	public Image Health;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Health = GetComponent<Image> ();
+
+	}
+	
+	public void LoseHealth()
+	{
+
+		Health.fillAmount -= 0.1f;
+	}
+
+	public void GainHealth()
+	{
+		Health.fillAmount += 0.1f;
+	}
+}
